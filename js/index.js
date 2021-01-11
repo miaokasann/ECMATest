a8bda94553f9a7369ac89f12e7a1c91fcb125d48
miaokasann <Wmg986410>
2021-01-11 22:04:21 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,9 +1,374 @@
-// function list(type) {
-// 	var result = "<" + type + "l><li>";
-// 	var args = Array.prototype.slice.call(arguments, 1);
-// 	result += args.join("</li><li>");
-// 	result += "</li></" + type + "l>"; // end list return result;
+/*
+    do{}while()
+*/
+// let start = 0;
+
+// do {
+//     let n = 0;
+//     do {
+//         document.write("*")
+//     } while (++n <= start)
+//     document.write("<br />")
+// } while (++start <= 5);
+
+/*
+    for循环杨辉三角
+*/
+// function hd(row = 5) {
+//     for(let i = 1; i < row; i++) {
+//         for(let m = row - i; m > 0; m--) {
+//             document.write("*");
+//         }
+//         for(let n = i * 2 - 1; n > 0; n--) {
+//             document.write("$");
+//         }
+//         document.write("<br />")
+//     }
 // }
+
+// hd();
+
+/*
+    break和continue
+*/
+
+// for (let i = 0; i <= 10; i++) {
+//     if(i % 2) continue;
+//     console.log(i);
+// }
+// let count = 0;
+// for (let i = 0; i <= 10; i++) {
+//     if(i % 2) {
+//         if(count++ == 3) break;
+//         console.log(i);
+//     }
+// }
+//
+
+/*
+    标签模板实例操作
+*/
+
+// let a = '王妙歌';
+// let web = 'miaokasann';
+
+// console.log(tag`你好${a}不好${web}非常好`);
+// function tag(strings, ...vars) {
+//     console.log(vars)
+//     console.log(strings)
+// }
+
+/*
+    字符串截取和查找
+*/
+// let str = 'miaokasann';
+// console.log(str.slice(0,6));
+// console.log(str.substr(0,6));
+// console.log(str.substring(0,6));
+
+// console.log(str.includes('m'))
+// console.log(str.indexOf('a', 1))
+// console.log(str.toUpperCase().startsWith('M'))
+// console.log(str.lastIndexOf('n', 9))
+
+/*
+    随机数
+*/
+
+// console.log(Math.random())
+//0-5
+// console.log(Math.floor(Math.random() * (5 + 1)))
+
+//2-5
+// console.log(2 + Math.floor(Math.random() * (5-2+1)))
+
+/*
+    时间戳,标准时间转换时间戳
+*/
+
+// const params = [1990, 3, 12, 14, 45, 12];
+// const date = new Date(...params);
+// console.log(...params);
+// console.log(date);
+
+// const date = new Date("1994-3-14 09:34:13");
+
+// console.log(date * 1)
+// console.log(Number(date))
+// console.log(date.valueOf())
+// console.log(date.getTime())
+
+// const timestamp = date.valueOf()
+// console.log(new Date(timestamp))
+
+/*
+    Array.of
+*/
+
+// let wmg = new Array(6,3,2)
+// console.log(wmg.length)
+// console.table(wmg)
+
+// let a = Array.of(6)
+// console.log(a)
+// console.table(a)
+
+/*
+    数据出栈入栈和填充操作
+*/
+
+// console.log(Array(5).fill("后盾人"));
+// console.log([1,2,3,4].fill("后盾人", 1, 3));
+
+/*
+    数组移动函数实例
+*/
+
+// function move(array, from, to) {
+//     if(from < 0 || to >= array.length) {
+//         console.error("参数错误");
+//         return
+//     }
+
+//     let newArray = [...array];
+//     let item = newArray.splice(from, 1);
+//     newArray.splice(to, 0, ...item);
+//     return newArray;
+// }
+
+// let array = [1, 2, 3, 4];
+
+// console.table(move(array, 2, 3))
+
+/*
+    清空数组
+*/
+
+// let hd = [1, 2, 3, 4, 5, 6, 7];
+// let arr = hd;
+
+// hd = [];
+// hd.pop()
+// hd.pop()
+// arr = []
+// console.log(arr);
+// console.log(hd);
+
+// console.log(hd.copyWithin(3, 1, 4))
+// 进行替换的目标的索引位置，要复制的开始索引位置，结束复制的位置索引
+
+// console.log(hd.lastIndexOf(2))
+//lastIndexOf从右侧开始查找
+
+/*
+    自定义includes方法
+*/
+
+// function includes(arr, find) {
+//     for(const value of arr)
+//     if(value === find) return true;
+//     return false;
+// }
+
+// let hd = [1, 2, 3, 4, 5, 6, 7]
+// console.log(includes(hd,4))
+
+/*
+    查找
+*/
+
+// let lesson = [
+//     {name: 'js'},
+//     {name: 'css'},
+//     {name: 'mysql'}
+// ];
+
+// console.log(lesson.includes({name: 'css'}));
+// //用inclues返回false
+
+// let status = lesson.find(item => {
+//     return item.name == 'css';
+// })
+
+// console.log(status);
+
+// let index = lesson.findIndex(item => {
+//     return item.name == 'css';
+// })
+
+// console.log(index)
+
+/*
+    自定义方法实现
+*/
+
+// function find(arr, callback) {
+//     for(const value of arr) {
+//         if(callback(value)) return value;
+//     }
+
+//     return undefined;
+// }
+
+// let arr = [1,2,3,4,5,6];
+
+// console.log(find(arr, function(item) {
+//     return item == 2
+// }))
+
+/*
+    数组排序
+*/
+
+// let arr = [1,5,3,6,9,12]
+// arr = arr.sort(function(a,b){
+//     return b - a;
+// })
+
+// console.table(arr)
+
+/*
+    sort排序原理 
+*/
+
+// function sort(arr, callback) {
+//     for(const m in arr) {
+//         for(const n in arr) {
+//             if(callback(arr[n], arr[m]) > 0) {
+//                 [arr[m], arr[n]] = [arr[n], arr[m]];
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(sort(arr, function(a, b) {
+//     return a - b;
+// }))
+
+/*
+    循环操作
+*/
+// let arr = [{n: 1}, {n: 2}];
+// for(let value of arr) {
+//     value.n += 10
+// }
+// console.log(arr)
+
+/*
+    迭代器 iterator
+*/
+
+// let arr = ['wmg', 'miaokasann'];
+// let keys = arr.keys()
+// let values = arr.values();
+
+// console.log(keys.next())
+// {value: 0, done: false} done代表是否迭代完成
+// let {value, done} = keys.next();
+
+// while(({value, done} = values.next()) && done === false) {
+//     console.log(value)
+// }
+
+// for (const key of arr.values()) {
+//     console.log(key)
+// }
+
+// let entries = arr.entries()
+// console.log(entries.next())
+// let {done, value: [a, b]} = entries.next();
+
+// console.log(a, b)
+
+// for(const [key, value] of arr.entries()) {
+//     console.log(value);
+// }
+
+/*
+    every和some
+*/
+
+// let res = arr.some(function(value, index, arr) {
+//     console.log(value)
+//     return true;
+// })
+
+// console.log(res)
+
+/*
+    自己实现filter
+*/
+
+// function filter(arr, callback) {
+//     let newArray = [];
+//     for(const value of arr) {
+//         if(callback(value) === true) {
+//             newArray.push(value)
+//         }
+//     }
+//     return newArray;
+// }
+
+// console.log(filter(arr, function(value) {
+//     return value === 'wmg';
+// }))
+
+/*
+    map映射数组
+*/
+
+// let arr = [
+//     {title: '王妙歌', age: '26'},
+//     {title: '孔德凯', age: '30'},
+//     {title: '张永梅', age: '52'}
+// ]
+
+// let hd = arr.map(function(value, index, arr) {
+//     // return Object.assign({click: 100}, value);
+//     return {
+//         title: value.title,
+//         age: value.age,
+//         click: 100
+//     }
+// })
+
+// console.log(hd)
+
+/*
+    reduce
+*/
+
+// let arr = [1,2,3,4,5];
+
+// arr.reduce(function(pre, value, index, array) {
+//     console.log(pre, value);
+//     return true;
+// })
+
+// arr.reduce(function(pre, value, index, array) {
+//     console.log(pre, value);
+//     return 99;
+// }, 0)
+
+//统计
+// function arraycount(array, item) {
+//     return array.reduce(function(total, cur) {
+//         total += cur == item ? 1 : 0;
+//         return total;
+//     }, 0)
+// }
+// let arr1 = [1,2,1,1,5];
+// console.log(arraycount(arr1, 1))
+
+//求和
+// function sum(arr) {
+//     return arr.reduce(function(total, cur) {
+//         return total += cur;
+//     }, 0);
+// }
+
+// console.log(sum(arr));
 
 /*
 	symbol
@@ -146,3 +511,5 @@
 // for (const [key, value] of map.entries()) {
 //   console.log(key, value)
 // }
+
+let a = 0
